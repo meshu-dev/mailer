c1af69268d36bf16dfa6112b49960d646ffb1885
meshu-dev <admin@requiredev.com>
2020-03-24 12:04:19 +0000

    Moved init code out of public folder

---
+++ app/init.php
@@ -0,0 +1,39 @@
+<?php
+use DI\ContainerBuilder;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+// Instantiate PHP-DI ContainerBuilder
+$containerBuilder = new ContainerBuilder();
+
+// Settings
+$settings = require __DIR__ . '/settings.php';
+$settings($containerBuilder);
+
+// Dependencies
+$dependencies = require __DIR__ . '/dependencies.php';
+$dependencies($containerBuilder);
+
+// Build PHP-DI Container instance
+$container = $containerBuilder->build();
+AppFactory::setContainer($container);
+
+$app = AppFactory::create();
+
+// Parse json, form data and xml
+$app->addBodyParsingMiddleware();
+
+// Middleware
+$middleware = require __DIR__ . '/middleware.php';
+$middleware($app);
+
+$routes = require __DIR__ . '/routes.php';
+$routes($app);
+
+require __DIR__ . '/cors.php';
+
+$app->run();
