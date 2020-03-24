1ffa3ee72d333f17a612d4609f0a059582fe93e8
meshu-dev <admin@requiredev.com>
2020-03-24 16:43:58 +0000

    Updated cors file to use sites from .env

--- app/cors.php
+++ app/cors.php
@@ -5,10 +5,24 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 
 $app->add(function ($request, $handler) {
     $response = $handler->handle($request);
-    return $response
-            ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
+    $allowedSites = getenv('ALLOWED_SITES');
+    $allowedSites = explode(',', $allowedSites);
+
+	if (in_array($_SERVER['HTTP_ORIGIN'], $allowedSites) === true) {
+	    $response = $response->withHeader(
+	    	'Access-Control-Allow-Origin',
+	    	$_SERVER['HTTP_ORIGIN']
+	    );
+	}
+
+    return $response->withHeader(
+    	'Access-Control-Allow-Headers',
+    	'X-Requested-With, Content-Type, Accept, Origin, Authorization'
+    )->withHeader(
+    	'Access-Control-Allow-Methods',
+    	'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    );
 });
 
 
